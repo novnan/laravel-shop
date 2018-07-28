3f05a70a900ba2c073bbc53da113767ee18a04fa
novnan <novnan@126.com>
2018-07-28 18:01:02 +0800

    添加布局文件

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,6 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 function test_helper()
 {
 	return 'OK';
+}
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
