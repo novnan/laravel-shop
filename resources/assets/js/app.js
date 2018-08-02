8389b04209605f7936be5e8c6d10180d3d1f9f33
novnan <novnan@126.com>
2018-08-02 10:33:29 +0800

    新增收货地址

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,8 +6,9 @@
  */
 
 require('./bootstrap');
-
 window.Vue = require('vue');
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
