673ce96c1ce181681e42388642473886969d8c02
novnan <novnan@126.com>
2018-08-03 10:38:22 +0800

    管理后台 - 新增、编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -60,10 +60,7 @@ class ProductsController extends Controller
     public function edit($id)
     {
         return Admin::content(function (Content $content) use ($id) {
-
-            $content->header('Edit');
-            $content->description('description');
-
+            $content->header('编辑商品');
             $content->body($this->form()->edit($id));
         });
     }
@@ -76,10 +73,7 @@ class ProductsController extends Controller
     public function create()
     {
         return Admin::content(function (Content $content) {
-
-            $content->header('Create');
-            $content->description('description');
-
+            $content->header('创建商品');
             $content->body($this->form());
         });
     }
@@ -121,12 +115,38 @@ class ProductsController extends Controller
      */
     protected function form()
     {
+        // 创建一个表单
         return Admin::form(Product::class, function (Form $form) {
-
-            $form->display('id', 'ID');
-
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+            $form->text('title', '商品名称')->rules('required');
+            // 创建一个选择图片的框
+            $form->image('image', '封面图片')->rules('required|image');
+            // 创建一个富文本编辑器
+            $form->editor('description', '商品描述')->rules('required');
+            // 创建一组单选框
+            $form->radio('on_sale', '上架')->options(['1' => '是', '0'=> '否'])->default('0');
+            // 直接添加一对多的关联模型
+            /*
+             * 在表单中直接添加一对多的关联模型，商品和商品 SKU 的关系就是一对多，第一个参数必须和主模型中定义此关联关系的方法同名，
+             * 我们之前在 App\Models\Product 类中定义了 skus() 方法来关联 SKU，因此这里我们需要填入 skus，第二个参数是对这个关联关系的描述，
+             * 第三个参数是一个匿名函数，用来定义关联模型的字段
+             */
+            $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
+                $form->text('title', 'SKU 名称')->rules('required');
+                $form->text('description', 'SKU 描述')->rules('required');
+                $form->text('price', '单价')->rules('required|numeric|min:0.01');
+                $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+            });
+            // 定义事件回调，当模型即将保存时会触发这个回调
+            /*
+             * 定义一个事件回调，当模型即将保存时会触发这个回调。我们需要在保存商品之前拿到所有 SKU 中最低的价格作为商品的价格，
+             * 然后通过 $form->model()->price 存入到商品模型中
+             * 当我们在前端移除一个 SKU 的之后，点击保存按钮时 Laravel-Admin 仍然会将被删除的 SKU 提交上去，但是会添加一个 _remove_=1 的字段
+             * 正常的 SKU 的 _remove_ 字段是 0，因此我们在查找最低价格时只需要查询 _remove_=0 的 SKU 即可
+             */
+            $form->saving(function (Form $form) {
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+            });
         });
     }
 }
